a9ca21dd96a37071c284912ec31991011b7b8ca5
Parichat <parichat@netkasystem.com>
2019-02-20 15:06:08 +0700

    ..

--- bot.php
+++ bot.php
@@ -66,9 +66,11 @@ function GetReplyMessage($text,$myUserId) {
 	$serviceUrl = 'http://vsmsdev.apps.thaibev.com/linebot/linebotWCF';
 	$groupWhereToGo = array('ไปไหน','อยู่ไหน','อยู่ไหม');
 	$groupWho = array('วสุต','นิน','เอิร์ธ','เอิท','โทนี่','เอ็กซ์','บะห์','อ้อ','ออย','วิท','เต้','น้อย','ดิว');
+	$groupWhen = array('วันนี้','พรุ่งนี้','เมื่อวาน');
 
 	$haveWhereToGo = searchGroup($groupWhereToGo,$text);
 	$who = searchGroup($groupWho,$text);
+	$when = searchGroup($groupWhen,$text);
 
 	
 	if(stripos($text, "ปิดบอท") !== false){
@@ -88,11 +90,14 @@ function GetReplyMessage($text,$myUserId) {
 			'text' => 'สวัสดีครับ'
 		]];
 	} 
-	else if ($haveWhereToGo  != "" && $who  != "" )
+	else if ($haveWhereToGo  != "" && $who  != "")
 	{
+		if($when == "")
+			$when = "วันนี้";
+		
 		$messages = [[
 			'type' => 'text',
-			'text' => $who . "ไปไหนสักแห่ง"
+			'text' => WhereToGo($who,$when)
 		]];
 	
 	}
@@ -100,7 +105,7 @@ function GetReplyMessage($text,$myUserId) {
 	{
 		$messages = [[
 			'type' => 'text',
-			'text' => 'ใครไปไหน'
+			'text' => 'ใคร' . $haveWhereToGo ;
 		]];
 	}
 	else if (stripos($text, "ป้อม") !== false) {
@@ -403,8 +408,9 @@ function searchGroup($ArrGroup,$text){
 	}
 	return $have_word ;
 }
-function CallNSD(){
+function WhereToGo($who,$when){
 
-	$response = file_get_contents('http://103.70.5.65/~haaohcom/nsd_bot/php/loadCust.php?cal_id=110');
+	$response = file_get_contents('http://103.70.5.65/~haaohcom/nsd_bot/php/loadBot.php?who=' . $who . '&when=' . $when);
+	return  $response;
 }
 echo "OK";
